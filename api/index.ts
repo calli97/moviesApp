47b11cd41a4abd3ceab3838e019fcd92917ed85c
calli97 <nahuelcallalli@gmail.com>
2023-06-19 17:40:28 -0300

    added typeorm

--- api/index.ts
+++ api/index.ts
@@ -1,5 +1,20 @@
 import app from "./src/app";
+import dotenv from "dotenv";
+import { AppDataSource } from "./src/dataSource";
+dotenv.config();
 
-app.listen(3000, () => {
-    console.log(`Server running on port 3000`);
-});
+const { PORT } = process.env;
+
+async function main() {
+    try {
+        await AppDataSource.initialize();
+        console.log("Database Connected");
+
+        app.listen(PORT);
+        console.log(`Server running on port: ${PORT}`);
+    } catch (error) {
+        console.error(error);
+    }
+}
+
+main();
---
+++ api/src/dataSource.ts
@@ -0,0 +1,19 @@
+import { DataSource } from "typeorm";
+import dotenv from "dotenv";
+dotenv.config();
+
+const { DB_HOST, DB_USER, DB_PASSWORD, DB_PORT, DB_NAME } = process.env;
+
+export const AppDataSource = new DataSource({
+    type: "mysql",
+    host: DB_HOST,
+    port: parseInt(DB_PORT ?? "3306"),
+    username: DB_USER,
+    password: DB_PASSWORD,
+    database: DB_NAME,
+    synchronize: true,
+    logging: true,
+    entities: [],
+    subscribers: [],
+    migrations: [],
+});
