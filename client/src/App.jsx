ac3fd6fc65fd63c2bdb1c210cdbe7bc6300453b5
calli97 <nahuelcallalli@gmail.com>
2023-06-20 13:07:11 -0300

    initialize client

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,17 @@
-import { useState } from "react";
-
 import "./App.css";
+import { Routes, Route } from "react-router-dom";
+import Landing from "./Pages/Landing";
+import Movies from "./Pages/Movies";
 
 function App() {
-    const [count, setCount] = useState(0);
-
-    return <div>Hola mundo</div>;
+    return (
+        <div>
+            <Routes>
+                <Route path="/" element={<Landing />} />
+                <Route path="/movies" element={<Movies />} />
+            </Routes>
+        </div>
+    );
 }
 
 export default App;
